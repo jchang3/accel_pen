6caf88fccebdd2c03ca872ae24d52ad98168957e
unknown <jchang3@e3-11-03.ece.ualberta.ca>
2014-03-13 17:54:06 -0600

    Added SDCard to SRAM test

---
+++ test/software/test_niosII_sdcard_to_sram/obj/default/hello_ucosii.d
@@ -0,0 +1,82 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/includes.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/os_cpu.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_irq.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/nios2.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/alt_types.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/linker.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/nios2.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/alt_types.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_irq.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//UCOSII/inc/os_cfg.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_alarm.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_llist.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/priv/alt_alarm.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//UCOSII/inc/ucos_ii.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//UCOSII/inc/os_cfg.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/os_cpu.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/alt_types.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_dev.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_llist.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/priv/alt_dev_llist.h \
+  C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_llist.h
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/includes.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/os_cpu.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_irq.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/nios2.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/alt_types.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/linker.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/nios2.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/alt_types.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_irq.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//UCOSII/inc/os_cfg.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_alarm.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_llist.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/priv/alt_alarm.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//UCOSII/inc/ucos_ii.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//UCOSII/inc/os_cfg.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/os_cpu.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp/system.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/alt_types.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_dev.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_llist.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+C:/Users/jchang3/Documents/accel_pen/test/software/test_niosII_sdcard_to_sram_bsp//HAL/inc/sys/alt_llist.h:
