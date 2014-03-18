d98534a2e907e81a3bcc9c9a8c91ec360e9f0d86
James <jchang3@ualberta.ca>
2014-03-18 13:14:44 -0600

    minor bugfix

---
+++ NiosII/SRAMToDTW/accel_pen_project/software/test_niosII_uart/obj/default/hello_ucosii.d
@@ -0,0 +1,105 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../test_niosII_uart_bsp//HAL/inc/includes.h \
+  ../test_niosII_uart_bsp//HAL/inc/os_cpu.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_irq.h \
+  ../test_niosII_uart_bsp//HAL/inc/nios2.h \
+  ../test_niosII_uart_bsp//HAL/inc/alt_types.h \
+  ../test_niosII_uart_bsp/system.h ../test_niosII_uart_bsp/linker.h \
+  ../test_niosII_uart_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../test_niosII_uart_bsp/system.h \
+  ../test_niosII_uart_bsp//HAL/inc/nios2.h \
+  ../test_niosII_uart_bsp//HAL/inc/alt_types.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_irq.h \
+  ../test_niosII_uart_bsp//UCOSII/inc/os_cfg.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_alarm.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_llist.h \
+  ../test_niosII_uart_bsp//HAL/inc/priv/alt_alarm.h \
+  ../test_niosII_uart_bsp/system.h \
+  ../test_niosII_uart_bsp//UCOSII/inc/ucos_ii.h \
+  ../test_niosII_uart_bsp//UCOSII/inc/os_cfg.h \
+  ../test_niosII_uart_bsp//HAL/inc/os_cpu.h \
+  ../test_niosII_uart_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_dev.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_llist.h \
+  ../test_niosII_uart_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_llist.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_alarm.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_warning.h \
+  ../test_niosII_uart_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../test_niosII_uart_bsp//HAL/inc/io.h \
+  ../test_niosII_uart_bsp//HAL/inc/alt_types.h \
+  ../test_niosII_uart_bsp//drivers/inc/altera_up_avalon_rs232.h \
+  ../test_niosII_uart_bsp//HAL/inc/alt_types.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_dev.h \
+  ../test_niosII_uart_bsp//drivers/inc/altera_up_avalon_rs232_regs.h \
+  ../test_niosII_uart_bsp//HAL/inc/sys/alt_irq.h
+
+../test_niosII_uart_bsp//HAL/inc/includes.h:
+
+../test_niosII_uart_bsp//HAL/inc/os_cpu.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_irq.h:
+
+../test_niosII_uart_bsp//HAL/inc/nios2.h:
+
+../test_niosII_uart_bsp//HAL/inc/alt_types.h:
+
+../test_niosII_uart_bsp/system.h:
+
+../test_niosII_uart_bsp/linker.h:
+
+../test_niosII_uart_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../test_niosII_uart_bsp/system.h:
+
+../test_niosII_uart_bsp//HAL/inc/nios2.h:
+
+../test_niosII_uart_bsp//HAL/inc/alt_types.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_irq.h:
+
+../test_niosII_uart_bsp//UCOSII/inc/os_cfg.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_alarm.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_llist.h:
+
+../test_niosII_uart_bsp//HAL/inc/priv/alt_alarm.h:
+
+../test_niosII_uart_bsp/system.h:
+
+../test_niosII_uart_bsp//UCOSII/inc/ucos_ii.h:
+
+../test_niosII_uart_bsp//UCOSII/inc/os_cfg.h:
+
+../test_niosII_uart_bsp//HAL/inc/os_cpu.h:
+
+../test_niosII_uart_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_dev.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_llist.h:
+
+../test_niosII_uart_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_llist.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_alarm.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_warning.h:
+
+../test_niosII_uart_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../test_niosII_uart_bsp//HAL/inc/io.h:
+
+../test_niosII_uart_bsp//HAL/inc/alt_types.h:
+
+../test_niosII_uart_bsp//drivers/inc/altera_up_avalon_rs232.h:
+
+../test_niosII_uart_bsp//HAL/inc/alt_types.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_dev.h:
+
+../test_niosII_uart_bsp//drivers/inc/altera_up_avalon_rs232_regs.h:
+
+../test_niosII_uart_bsp//HAL/inc/sys/alt_irq.h:
