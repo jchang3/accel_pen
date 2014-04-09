554169dfc78bc6364561517656ee4c5956a04638
James <jchang3@ualberta.ca>
2014-04-09 14:37:17 -0600

    Removed excess directories

---
+++ test/software/accelerometer_pen50sflash/obj/default/accelerometer_pen50sflash.d
@@ -0,0 +1,115 @@
+obj/default/accelerometer_pen50sflash.o: accelerometer_pen50sflash.c \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/includes.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/os_cpu.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_irq.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/nios2.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/linker.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/nios2.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_irq.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//UCOSII/inc/os_cfg.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_alarm.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_llist.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/priv/alt_alarm.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//UCOSII/inc/ucos_ii.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//UCOSII/inc/os_cfg.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/os_cpu.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_dev.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_llist.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/priv/alt_dev_llist.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_llist.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_alarm.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_warning.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/io.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_avalon_rs232.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_dev.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_avalon_rs232_regs.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h \
+  dtw.h \
+  C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_cache.h
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/includes.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/os_cpu.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_irq.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/nios2.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/linker.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/nios2.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_irq.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//UCOSII/inc/os_cfg.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_alarm.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_llist.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/priv/alt_alarm.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//UCOSII/inc/ucos_ii.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//UCOSII/inc/os_cfg.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/os_cpu.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp/system.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_dev.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_llist.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_llist.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_alarm.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_warning.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/io.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_avalon_rs232.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/alt_types.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_dev.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_avalon_rs232_regs.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h:
+
+dtw.h:
+
+C:/Users/jchang3/Documents/ECE492/test/software/accelerometer_pen50sflash_bsp//HAL/inc/sys/alt_cache.h:
